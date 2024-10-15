a2a5c46f1925f1ef3e07c17f9d48ad71d6698f18
abdul rohman <abdulrohmanunida@gmail.com>
2024-10-15 17:25:32 +0700

    Union dan Intersection Types

--- index.ts
+++ index.ts
@@ -14,3 +14,34 @@
 // vAny.asta()
 // vUnknown.asta()
 
+//3. Union dan Intersection Types
+
+//union
+let numberOrString: string | number | boolean = "asdasd";
+numberOrString = true;
+
+//intersection
+type A = {
+  username: string;
+  age?: number;
+};
+
+type B = {
+  fullName: string;
+};
+
+type C = A & B;
+
+const user: A = {
+  age: 25,
+  username: "Asta",
+};
+
+const user2: B = {
+  fullName: "Black Clover",
+};
+
+const user3: C = {
+  username: "asta2",
+  fullName: "black clover2",
+};
