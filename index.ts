0d26fba5bbd301c141683dcb1ed1c8d6e0f31958
abdul rohman <abdulrohmanunida@gmail.com>
2024-10-15 17:00:55 +0700

    Type Any dan Unknown

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-const age: number = 25;
-const fullName: string = "Naruto";
-const isStudent: boolean = true;
+// 1. Primitive Types
+// const age: number = 25;
+// const fullName: string = "Naruto";
+// const isStudent: boolean = true;
+
+//2. Type Any dan Unknown
+// const vAny: any = 10;
+// const vUnknown: unknown = 10;
+
+// const data1: string = vAny;
+// const data2: number = vUnknown as number;
+// const data3: string = vUnknown as string;
+
+// vAny.asta()
+// vUnknown.asta()
+
